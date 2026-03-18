2f59bc067111dd30679f77a557e7b82df71252e6
Sukumar dey <dey.sukumar1999@gmail.com>
2026-03-18 17:04:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 //your JS code here. If required.
  const table_body = document.getElementById("output")
  table_body.innerHTML = `<tr>
-			  <td colspan=2 > Loading...</td>
+			  <td colspan=2 id="loading"> Loading...</td>
 		  </tr>`
 
 let promise1 = new Promise((res,rej)=>{
-	let time = Math.floor(Math.random()*3000)
+	let time = Math.floor(Math.random()*2000)+1000
 	setTimeout(()=>{
-		res(Math.ceil(time/1000))
+		res(time)
 	},time)
 })
 let promise2 = new Promise((res,rej)=>{
-	let time = Math.floor(Math.random()*3000)
+	let time = Math.floor(Math.random()*2000)+1000
 	setTimeout(()=>{
-		res(Math.ceil(time/1000))
+		res(time)
 	},time)
 })
 let promise3 = new Promise((res,rej)=>{
-	let time = Math.floor(Math.random()*3000)
+	let time = Math.floor(Math.random()*2000)+1000
 	setTimeout(()=>{
-		res(Math.ceil(time/1000))
+		res(time)
 	},time)
 })
 
@@ -27,15 +27,19 @@ Promise.all([promise1,promise2,promise3])
 	.then(([r1,r2,r3])=>{
 		table_body.innerHTML = `<tr>
 			  <td>Promise 1</td>
-			  <td>${r1.toFixed(3)}</td>
+			  <td>${(r1/1000).toFixed(3)}</td>
 		  </tr><tr>
 			  <td>Promise 2</td>
-			  <td>${r2.toFixed(3)}</td>
+			  <td>${(r2/1000).toFixed(3)}</td>
 		  </tr><tr>
 			  <td>Promise 3</td>
-			  <td>${r3.toFixed(3)}</td>
+			  <td>${(r3/1000).toFixed(3)}</td>
 		  </tr><tr>
 			  <td>Total</td>
-			  <td>${Math.max(r1,r2,r3).toFixed(3)}</td>
+			  <td>${(Math.max(r1,r2,r3)/1000).toFixed(3)}</td>
 		  </tr>`
+	}).catch((e)=>{
+	table_body.innerHTML = `<tr>
+			  <td colspan=2 id="loading"> Loading...</td>
+		  </tr>
 	})
